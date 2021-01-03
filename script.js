51b34747ab2c4e0ede15951acfe2f0ff7562708e
QuentinHck <quentin.haerinck@gmail.com>
2021-01-03 14:57:54 +0100

    latest version 03/01

--- script.js
+++ script.js
@@ -143,5 +143,88 @@ window.onscroll = function () {
     
 };
 
-
 document.addEventListener('click',dropDownEffect);
+
+/*--------------------SLIDER------------------*/
+
+const previousBtn = document.querySelector(".sliderPreviousBtn");
+const nextBtn = document.querySelector(".sliderNextBtn");
+const sliderContainer = document.querySelector(".evenementen-slider__wrapper");
+const slider = document.querySelector(".evenementen-slider__wrapper");
+
+let sliderInfo = {
+  sliderElement: sliderContainer,
+  sliderItemCount: document.querySelector(".evenementen-slider__wrapper"),
+  currentSlide: 0
+};
+
+function getSliderItemWidth() {
+  const sliderItem = document.querySelector(".evenementen-slider__item");
+
+  return sliderItem.clientWidth;
+}
+
+function setSliderPosition() {
+  const itemWidth = getSliderItemWidth();
+  sliderInfo.sliderElement.style.left =
+    -1 * sliderInfo.currentSlide * itemWidth + "px";
+}
+
+function getShownItems() {
+  const container = document.querySelector(".subcontainerSlider");
+
+  return Math.floor(container.clientWidth / getSliderItemWidth());
+}
+
+function nextSlide(event) {
+  const shownItems = getShownItems();
+
+  if (sliderInfo.currentSlide === sliderInfo.sliderItemCount - shownItems) {
+    nextBtn.classList.add("hide");
+    return;
+  }
+
+  if (
+    sliderInfo.currentSlide ===
+    sliderInfo.sliderItemCount - (shownItems + 1)
+  ) {
+    nextBtn.classList.add("hide");
+  }
+
+  previousBtn.classList.remove("hide");
+
+  sliderInfo.currentSlide++;
+
+  setSliderPosition();
+}
+
+function previousSlide(event) {
+  if (sliderInfo.currentSlide === 0) {
+    return;
+  }
+
+  if (sliderInfo.currentSlide === 1) {
+    previousBtn.classList.add("hide");
+  }
+
+  nextBtn.classList.remove("hide");
+  sliderInfo.currentSlide--;
+
+  setSliderPosition();
+}
+
+function windowResize() {
+  setSliderPosition();
+
+  const shownItems = getShownItems();
+
+  if (sliderInfo.currentSlide === sliderInfo.sliderItemCount - shownItems) {
+    nextBtn.classList.add("hide");
+  } else {
+    nextBtn.classList.remove("hide");
+  }
+}
+
+previousBtn.addEventListener("click", previousSlide);
+nextBtn.addEventListener("click", nextSlide);
+window.addEventListener("resize", windowResize);
