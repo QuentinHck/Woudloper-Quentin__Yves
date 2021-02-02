67e256c575b2684cef4b9e737b1e2f2f35242bbc
Korneel Eeckhout <korneel@pienter.tech>
2021-02-02 20:09:22 +0100

    clean doubles

--- main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import '@/assets/style/main.css';
-import '@/assets/js/main.js';
-
-createApp(App)
-  .use(router)
-  .mount("#app");
