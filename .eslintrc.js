061e0a6b63533cedceabf3d978bac9324f5ae678
Korneel Eeckhout <korneel@pienter.tech>
2021-02-02 21:11:17 +0100

    header component

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,23 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
-  extends: ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/prettier"],
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/prettier'],
   parserOptions: {
-    parser: "babel-eslint"
+    parser: 'babel-eslint',
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "prettier/prettier": [
-      "warn",
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'prettier/prettier': [
+      'warn',
       {
-        trailingComma: "es5",
+        trailingComma: 'all',
         tabWidth: 2,
         semi: true,
         singleQuote: true,
       },
-    ]
-  }
-  
+    ],
+  },
 };
---
+++ src/components/Header.vue
@@ -0,0 +1,74 @@
+<template>
+  <div id="navigation" class="navigation-background__transparant">
+    <div class="dropdown__navbar container">
+      <img src="@/assets/images/logo.png" alt="logo scouts woudloper" />
+      <div :class="{ 'dropdown-menu': true, open: dropdownOpen }">
+        <button
+          @click="toggleDropdown"
+          class="dropdown-menu__button-container"
+          type="button"
+        >
+          <span class="dropdown-menu__button"></span>
+        </button>
+        <div
+          :class="{
+            'dropdown-menu__list': true,
+            'show-dropdown': dropdownOpen,
+          }"
+        >
+          <ul class="dropdown-menu__list-content">
+            <li v-for="navLink in navLinks" :key="navLink.path">
+              <router-link
+                :class="{ active: $route.path === navLink.path }"
+                :to="navLink.path"
+              >
+                {{ navLink.label }}
+              </router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <nav class="container">
+      <img src="@/assets/images/logo.png" alt="logo scouts woudloper" />
+      <ul>
+        <li v-for="navLink in navLinks" :key="navLink.path">
+          <router-link
+            :class="{ active: $route.path === navLink.path }"
+            :to="navLink.path"
+          >
+            {{ navLink.label }}
+          </router-link>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data() {
+    return {
+      dropdownOpen: false,
+      navLinks: [
+        { label: 'Home', path: '/' },
+        { label: 'Evenementen', path: '/evenementen/' },
+        { label: 'Nieuws', path: '/nieuws/' },
+        { label: 'Contact', path: '/contact/' },
+      ],
+    };
+  },
+  methods: {
+    toggleDropdown() {
+      this.dropdownOpen = !this.dropdownOpen;
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+#navigation {
+  background-color: #fff;
+}
+</style>
