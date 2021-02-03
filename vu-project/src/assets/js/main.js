e12a036f16ef29d950db4fd0b30ce8c5ec45b763
stephane_mahop <tatosteph@gmail.com>
2021-02-03 23:19:39 +0100

    init

--- vu-project/src/assets/js/main.js
+++ vu-project/src/assets/js/main.js
@@ -1,3 +1,4 @@
+/*
 // agenda
 let today = new Date();
 let currentMonth = today.getMonth();
@@ -17,26 +18,25 @@ let months = [
   "Dec"
 ];
 let monthAndYear = document.getElementById('monthAndYear');
-showCalendar(currentMonth, currentYear);
-
-function showCalendar(month, year) {
-  let firstDay = new Date(year, month).getDay();
+showCalendar(currentMonth,currentYear);
+function showCalendar(month, year){
+  let firstDay= new Date(year, month).getDay();
   let daysInMonth = 32 - new Date(year, month, 32).getDate();
   let tbl = document.getElementById('calendar-body');
   tbl.innerHTML = "";
   monthAndYear.innerHTML = months[month] + " " + year;
-  let date = 1;
-  for (let i = 0; i < 6; i++) {
+  let date = 1; 
+  for (let i = 0 ; i < 6; i++){
     let row = document.createElement('tr');
-    for (let j = 0; j < 7; j++) {
-      if (i === 0 && j < firstDay) {
+    for(let j = 0; j < 7; j++){
+      if(i === 0 && j < firstDay){
         let cell = document.createElement('td');
         let cellText = document.createTextNode("");
         cell.appendChild(cellText);
         row.appendChild(cell);
-      } else if (date > daysInMonth) {
+      }else if(date > daysInMonth){
         break;
-      } else {
+      }else{
         let cell = document.createElement("td");
         let cellText = document.createTextNode(date);
         cell.appendChild(cellText);
@@ -47,17 +47,15 @@ function showCalendar(month, year) {
     tbl.appendChild(row);
   }
 }
-
-function calendarPrev() {
-  currentYear = (currentMonth === 0) ? currentYear - 1 : currentYear;
-  currentMonth = currentMonth === 0 ? 11 : currentMonth - 1;
-  showCalendar(currentMonth, currentYear);
+function calendarPrev(){
+  currentYear = (currentMonth === 0)? currentYear - 1 : currentYear;
+  currentMonth = currentMonth === 0? 11: currentMonth - 1;
+  showCalendar(currentMonth,currentYear);
 }
-
-function calendarNext() {
+function calendarNext(){
   currentYear = currentMonth === 11 ? currentYear + 1 : currentYear;
   currentMonth = (currentMonth + 1) % 12;
-  showCalendar(currentMonth, currentYear);
+  showCalendar(currentMonth,currentYear);
 }
 // toggle dropdown-menu
 const $dropdownContainer = document.querySelector('.dropdown-menu');
@@ -73,14 +71,13 @@ const $cardCalendar = document.getElementById('card-calendar');
 let $navBar = document.getElementById('navigation');
 let $aside = document.querySelector('.signup-field');
 let menuOpen = false;
-
-function openDropdown(target1, target2) {
-  if (!menuOpen) {
+function openDropdown(target1,target2){
+  if(!menuOpen ){
     target1.classList.add('open');
     target2.classList.add('show-dropdown');
     $aside.classList.remove('reduce');
     menuOpen = true;
-  } else if (menuOpen && document.documentElement.scrollTop >= 70) {
+  }else if( menuOpen && document.documentElement.scrollTop >= 70){
     target1.classList.remove('open');
     target2.classList.remove('show-dropdown');
     menuOpen = false;
@@ -124,67 +121,67 @@ window.onscroll = function () {
 };
 document.addEventListener('click',dropDownEffect);
 /*--------------------SLIDER------------------*/
-  /*
-  const previousBtn = document.querySelector(".evenementen-slider-buttons__left");
-  const nextBtn = document.querySelector(".evenementen-slider-buttons__right");
-  const sliderContainer = document.querySelector(".evenementen-slider__frame");
-  const slider = document.querySelector(".evenementen-slider__wrapper");
-  let sliderInfo = {
-    sliderElement: sliderContainer,
-    sliderItemCount: document.querySelector(".evenementen-slider__wrapper"),
-    currentSlide: 0
-  };
-  function getSliderItemWidth() {
-    const sliderItem = document.querySelector(".evenementen-slider__item");
-    return sliderItem.clientWidth;
+/*
+const previousBtn = document.querySelector(".evenementen-slider-buttons__left");
+const nextBtn = document.querySelector(".evenementen-slider-buttons__right");
+const sliderContainer = document.querySelector(".evenementen-slider__frame");
+const slider = document.querySelector(".evenementen-slider__wrapper");
+let sliderInfo = {
+  sliderElement: sliderContainer,
+  sliderItemCount: document.querySelector(".evenementen-slider__wrapper"),
+  currentSlide: 0
+};
+function getSliderItemWidth() {
+  const sliderItem = document.querySelector(".evenementen-slider__item");
+  return sliderItem.clientWidth;
+}
+function setSliderPosition() {
+  const itemWidth = getSliderItemWidth();
+  sliderInfo.sliderElement.style.left =
+    -1 * sliderInfo.currentSlide * itemWidth + "px";
+}
+function getShownItems() {
+  const container = document.querySelector(".evenementen-slider__subcontainer");
+  return Math.floor(container.clientWidth / getSliderItemWidth());
+}
+function nextSlide(event) {
+  const shownItems = getShownItems();
+  if (sliderInfo.currentSlide === sliderInfo.sliderItemCount - shownItems) {
+    nextBtn.classList.add("hide");
+    return;
   }
-  function setSliderPosition() {
-    const itemWidth = getSliderItemWidth();
-    sliderInfo.sliderElement.style.left =
-      -1 * sliderInfo.currentSlide * itemWidth + "px";
+  if (
+    sliderInfo.currentSlide ===
+    sliderInfo.sliderItemCount - (shownItems + 1)
+  ) {
+    nextBtn.classList.add("hide");
   }
-  function getShownItems() {
-    const container = document.querySelector(".evenementen-slider__subcontainer");
-    return Math.floor(container.clientWidth / getSliderItemWidth());
+  previousBtn.classList.remove("hide");
+  sliderInfo.currentSlide++;
+  setSliderPosition();
+}
+function previousSlide(event) {
+  if (sliderInfo.currentSlide === 0) {
+    return;
   }
-  function nextSlide(event) {
-    const shownItems = getShownItems();
-    if (sliderInfo.currentSlide === sliderInfo.sliderItemCount - shownItems) {
-      nextBtn.classList.add("hide");
-      return;
-    }
-    if (
-      sliderInfo.currentSlide ===
-      sliderInfo.sliderItemCount - (shownItems + 1)
-    ) {
-      nextBtn.classList.add("hide");
-    }
-    previousBtn.classList.remove("hide");
-    sliderInfo.currentSlide++;
-    setSliderPosition();
+  if (sliderInfo.currentSlide === 1) {
+    previousBtn.classList.add("hide");
   }
-  function previousSlide(event) {
-    if (sliderInfo.currentSlide === 0) {
-      return;
-    }
-    if (sliderInfo.currentSlide === 1) {
-      previousBtn.classList.add("hide");
-    }
+  nextBtn.classList.remove("hide");
+  sliderInfo.currentSlide--;
+  setSliderPosition();
+}
+function windowResize() {
+  setSliderPosition();
+  const shownItems = getShownItems();
+  if (sliderInfo.currentSlide === sliderInfo.sliderItemCount - shownItems) {
+    nextBtn.classList.add("hide");
+  } else {
     nextBtn.classList.remove("hide");
-    sliderInfo.currentSlide--;
-    setSliderPosition();
   }
-  function windowResize() {
-    setSliderPosition();
-    const shownItems = getShownItems();
-    if (sliderInfo.currentSlide === sliderInfo.sliderItemCount - shownItems) {
-      nextBtn.classList.add("hide");
-    } else {
-      nextBtn.classList.remove("hide");
-    }
-  }
-  /*----- event listeners----*/
-  /*
-  previousBtn.addEventListener("click", previousSlide);
-  nextBtn.addEventListener("click", nextSlide);
-  window.addEventListener("resize", windowResize);*/
+}
+/*----- event listeners----*/
+/*
+previousBtn.addEventListener("click", previousSlide);
+nextBtn.addEventListener("click", nextSlide);
+window.addEventListener("resize", windowResize);*/
--- vu-project/src/main.js
+++ vu-project/src/main.js
@@ -1,5 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import {
+  createApp
+} from "vue";
+import App from "./App.vue";
+import router from "./router";
+import '@/assets/style/main.css';
+import '@/assets/js/main.js';
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+  .use(router)
+  .mount("#app");
