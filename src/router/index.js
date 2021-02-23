0b4406ac579a5629b7171504de0b4391f7ff678a
Korneel Eeckhout <korneel@pienter.tech>
2021-02-23 20:18:59 +0100

    merge-yves-quentin

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Index from '@/views/Index';
 import Evenementen from '@/views/Evenementen';
 import Nieuws from '@/views/Nieuws';
 import Contact from '@/views/Contact';
+import About from '@/views/About';
 
 const routes = [
   {
@@ -19,6 +20,11 @@ const routes = [
     path: '/nieuws',
     name: 'Nieuws',
     component: Nieuws,
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: About,
   },
   {
     path: '/contact',
